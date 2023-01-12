12cf2f674a3a855caccd5e46475bbe5a9c908b13
Sebastian A. Brachi <sebastian@brachi.org>
2023-01-11 23:35:46 -0300

    Capture exceptions decompressing from arc

--- albam_reloaded/engines/mtframework/archive.py
+++ albam_reloaded/engines/mtframework/archive.py
@@ -6,24 +6,24 @@ import bpy
 from kaitaistruct import KaitaiStream
 
 from albam_reloaded.registry import blender_registry
-from . import KNOWN_ARC_BLENDER_CRASH, CORRUPTED_ARCS, EXTENSION_TO_FILE_ID
+from . import EXTENSION_TO_FILE_ID
 from .mesh import build_blender_model
 from .structs.arc import Arc
 
 
 @blender_registry.register_function("import", identifier=b"ARC\x00")
 def import_arc(file_path):
-    if file_path.endswith(tuple(KNOWN_ARC_BLENDER_CRASH) + tuple(CORRUPTED_ARCS)):
-        raise ValueError("The arc file provided is not supported yet, it might crash Blender")
-
     bl_arc_container = bpy.data.objects.new(os.path.basename(file_path), None)
     arc = ArcWrapper(file_path)
     mod_type = EXTENSION_TO_FILE_ID['mod']
     mod_file_entries = arc.get_file_entries_by_type(mod_type)
 
     for mod_file_entry in mod_file_entries:
-        bl_mod_container = build_blender_model(arc, mod_file_entry)
-        bl_mod_container.parent = bl_arc_container
+        try:
+            bl_mod_container = build_blender_model(arc, mod_file_entry)
+            bl_mod_container.parent = bl_arc_container
+        except Exception as err:
+            print("failed to build model", mod_file_entry.file_path, err)
 
     return bl_arc_container
 
@@ -46,6 +46,10 @@ class ArcWrapper:
 
         for fe in self.parsed.file_entries:
             if fe.file_path == file_path and fe.file_type == file_type:
-                file_ = zlib.decompress(fe.raw_data)
-                break
+                try:
+                    file_ = zlib.decompress(fe.raw_data)
+                    break
+                except EOFError:
+                    print(f"Requested to read out of bounds. Offset: {fe.offset}")
+                    raise
         return file_
